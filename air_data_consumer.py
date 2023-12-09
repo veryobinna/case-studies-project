859f3d0f02936dd8b350039ccf426996bbc56db7
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2023-12-09 17:44:02 +0100

    get data from API and store in mongo db

--- air_data_consumer.py
+++ air_data_consumer.py
@@ -1,10 +1,22 @@
 from kafka.consumer import KafkaConsumer
+from db import database
+
+collection_name = "water_pollution_data"
+
+collection = database[collection_name]
 
 brokers = "localhost:9092"
 topic = "case-project-air-data"
 
 consumer = KafkaConsumer(topic, bootstrap_servers=brokers)
 
-for msg in consumer:
-    print(msg)
-    print("*"*50)
+def extract_data(resp):
+    components = resp["components"]
+    components["datetime"] = resp["dt"]
+    return components
+    
+
+for resp in consumer:
+    data = extract_data(resp)
+    collection.insert_one(data)
+    print("*"*50,"air data inserted","*"*50)
--- producer.py
+++ producer.py
@@ -3,11 +3,19 @@ import requests
 import json
 import time
 
-brokers = "localhost:9092"
-water_data_topic = "case-project-water-data"
-air_data_topic = "case-project-air-data"
+from my_secrets import water_data_api_key, air_data_api_key
 
-producer = KafkaProducer(bootstrap_servers=brokers)
+
+LONGITUDE = "55.4920"
+LATITUDE = "4.6796"
+AIR_DATA_API_URL = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={LATITUDE}&lon={LONGITUDE}&appid={air_data_api_key}"
+WATER_DATA_API_URL = f"https://api.meersens.com/environment/public/water/current?lat={LATITUDE}&lng={LONGITUDE}"
+
+BROKERS = "localhost:9092"
+WATER_DATA_TOPIC = "case-project-water-data"
+AIR_DATA_TOPIC = "case-project-air-data"
+
+producer = KafkaProducer(bootstrap_servers=BROKERS)
 
 
 def get_data(topic, api_url):
@@ -22,8 +30,8 @@ def get_data(topic, api_url):
 def start_producer():
     count = 0
     while count < 15:
-        get_data(topic=water_data_topic, api_url="https://randomuser.me/api")
-        get_data(topic=air_data_topic, api_url="https://randomuser.me/api")
+        get_data(topic=WATER_DATA_TOPIC, api_url=WATER_DATA_API_URL, headers={"apikey": water_data_api_key})
+        get_data(topic=AIR_DATA_TOPIC, api_url=AIR_DATA_API_URL)
 
         time.sleep(10)
         count += 1
